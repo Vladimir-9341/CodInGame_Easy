116e3f62f6c8bba20cf1a4a1f2fbf41ab4b3a9aa
Vladimir <39557447+Vladimir-9341@users.noreply.github.com>
2018-07-11 20:26:35 +0300

    Лямбды рулят

---
+++ HorseRacingDual.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] str = new int[N];
+            for (int i = 0; i < N; i++)
+                str[i] = int.Parse(Console.ReadLine());
+            Array.Sort(str);
+            str = str.Select((c, d) => (str.Length > d + 1 ? str[d + 1] - c : -1)).ToArray();
+            Array.Sort(str);
+            Console.Write(str[1]);
+        }
+    }
+}
