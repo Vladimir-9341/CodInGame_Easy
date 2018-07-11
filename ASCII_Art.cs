c81799b18bc5c9d760a40d1b6699f14b90ffad81
Vladimir <39557447+Vladimir-9341@users.noreply.github.com>
2018-07-11 20:37:19 +0300

    Add files via upload

---
+++ ASCII_Art.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int L = int.Parse(Console.ReadLine());
+            int H = int.Parse(Console.ReadLine());
+            string T = Console.ReadLine();
+
+            const int A = (int)'A';
+            const int Z = (int)'Z';
+
+            string str = T.ToUpper();
+
+            for (int i = 0; i < H; i++)
+            {
+
+                string row = Console.ReadLine();
+                string ret = "";
+
+                foreach (var s in str)
+                {
+                    var c = (int)s;
+
+                    if (c < A || c > Z)
+                        ret += row.Substring(L * (Z - A + 1), L);
+                    else
+                        ret += row.Substring(L * (c - A), L);
+                }
+                Console.WriteLine(ret);
+            }
+        }
+    }
+
+}
